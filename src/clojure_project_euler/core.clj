99a2e56b72ee4902e9d0743cc976cafecff0cdba
Ron Hopper <rchopper@gmail.com>
2013-07-17 16:23:40 -0400

    print problem solutions in order

--- src/clojure_project_euler/core.clj
+++ src/clojure_project_euler/core.clj
@@ -7,7 +7,7 @@
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
 
-  (def files (remove #(.isDirectory %) (file-seq (clojure.java.io/file "src/clojure_project_euler/problems"))))
+  (def files (sort (remove #(.isDirectory %) (file-seq (clojure.java.io/file "src/clojure_project_euler/problems")))))
 
   (doseq [f files]
     (println (str "Problem " (clojure.string/replace (.getName f) #"_.*" "") ": " (load-file (str f)))))
