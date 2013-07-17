3916ad0b2a0cfca434a83857ff85846d46f365e5
Ron Hopper <rchopper@gmail.com>
2013-07-17 14:11:38 -0400

    generated initial app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-project-euler
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clojure_project_euler/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-project-euler.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-project-euler.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
